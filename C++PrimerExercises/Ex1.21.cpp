f1ff1b55477d01bf556044fd8c6ba037d16458a5
vegeta <657828203.qq.com>
2015-10-11 00:42:03 +0800

    modified Ex1.21.cpp Ex1.22.cpp.

--- C++PrimerExercises/Ex1.21.cpp
+++ C++PrimerExercises/Ex1.21.cpp
@@ -3,10 +3,15 @@
 int main()
 {
 	Sales_item item1,item2;
-	std::cout<<"pls enter two transactions:\n";
 	std::cin>>item1>>item2;
-	std::cout<<item1+item2<<std::endl;
-	return 0;
+	if(item1.isbn()==item2.isbn())
+	{
+		std::cout<<item1+item2<<std::endl;
+		return 0;
+	}else{
+		std::cerr<<"Data must refer to the same ISBN.\n";
+		return -1;
+		}	
 }
 //here is one problem:I enter two transactions with different ISBNs,
 //whereas the item1+item2 result still is printed as if they have the same ISBN,
--- C++PrimerExercises/Ex1.22.cpp
+++ C++PrimerExercises/Ex1.22.cpp
@@ -2,9 +2,24 @@
 #include "Sales_item.h"
 int main()
 {
-	Sales_item item,SUM;
-	std::cout<<"pls enter the transactions:\n";
-	for(;std::cin>>item;)SUM+=item;
-	std::cout<<"SUM=\n"<<SUM<<std::endl;
+	Sales_item sum;
+	if(std::cin>>sum)
+	{
+		Sales_item item;  //here,when I put item outside if together
+                                  //with sum,the running result always goes                                        // wrong. why?
+		while(std::cin>>item)
+		{
+			if(sum.isbn()==item.isbn())sum+=item;
+			else{
+			std::cout<<sum<<std::endl;
+			sum=item;
+			}
+		}
+	std::cout<<sum<<std::endl;
+	}
+	else{
+	std::cerr<<"No data? :P"<<std::endl;
+	return -1;
+	}
 	return 0;
 }
