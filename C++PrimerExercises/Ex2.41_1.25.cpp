9fd8ec24ce977baf43af7225880e1657c572db09
Corrado <1526952081@qq.com>
2015-11-03 01:05:55 +0800

    I added Ex2.41_1.25.cpp and Sales_data.h tonight.

---
+++ C++PrimerExercises/Ex2.41_1.25.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+struct Sales_data {
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+int main(){
+	Sales_data total;
+	double price = 0.0;
+	if (std::cin >> total.bookNo >> total.units_sold >> price){
+		total.revenue = total.units_sold * price;
+		Sales_data trans;
+		while (std::cin >> trans.bookNo >> trans.units_sold >> price){
+		trans.revenue = trans.units_sold * price;
+		if (total.bookNo == trans.bookNo)
+		{
+			total.units_sold += trans.units_sold;
+			total.revenue += trans.revenue;
+		}
+		else{
+		std::cout << '\n' << total.bookNo << " " << total.units_sold << " " << total.revenue << " " << total.revenue/total.units_sold
+			  << std::endl;
+		total.bookNo = trans.bookNo;
+		total.units_sold = trans.units_sold;
+		total.revenue = trans.revenue;
+		}
+	}
+	std::cout << '\n' << total.bookNo << " " << total.units_sold << " " << total.revenue << " " << total.revenue/total.units_sold << std::endl;
+	}else{
+	std::cerr << "No data?!" << std ::endl;
+	return -1;
+	}
+	return 0;
+}		
