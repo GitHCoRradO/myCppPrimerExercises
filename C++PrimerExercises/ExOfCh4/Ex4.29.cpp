929b55265e3b3121e8322c2fa6c47c6d6bd875a2
Corrado <1526952081@qq.com>
2016-03-03 23:32:44 +0800

    modified Ex4.29.cpp.

--- C++PrimerExercises/ExOfCh4/Ex4.29.cpp
+++ C++PrimerExercises/ExOfCh4/Ex4.29.cpp
@@ -5,6 +5,6 @@ int main() {
 	int x[10];
 	int *p = x;
 	cout << sizeof(x)/sizeof(*x) << endl;	//prints 10.
-	cout << sizeof(p)/sizeof(*p) << endl;	//no idea about the result.the result printed by my machine is 1. here is a reference:https://github.com/GitHCoRradO/Cpp-Primer/tree/master/ch04
+	cout << sizeof(p)/sizeof(*p) << endl;	//no idea about the result.the result printed by my machine is 1. here is a reference:http://stackoverflow.com/questions/2428765/why-the-size-of-a-pointer-is-4bytes-in-c/2428809#2428809
 	return 0;
 }
